e075a9758c9c1ca4cd66f6bf46614116d56a13ca
Fahemi <fahemi1313@gmail.com>
2023-04-02 14:39:51 +0530

    I have added a new feature called webapp

---
+++ django-student-management-system-master/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\fahem\downloads\django-student-management-system-master\django-student-management-system-master\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
